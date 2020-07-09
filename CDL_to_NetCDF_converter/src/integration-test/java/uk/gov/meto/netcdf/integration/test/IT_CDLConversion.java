1bea7a0ae1d681ee6ed6b67f2b5cda6499627620
cnorris <charlotte.norris@metoffice.gov.uk>
2020-07-09 15:01:01 +0100

    Application now converts .cdl file with ncgen, integration test runs app and reads resulting .nc file. Resource loading implemented. Attempts made to produce logs.

--- CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
+++ CDL_to_NetCDF_converter/src/integration-test/java/uk/gov/meto/netcdf/integration/test/IT_CDLConversion.java
@@ -3,13 +3,50 @@ package uk.gov.meto.netcdf.integration.test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.gov.meto.netcdf.cdl.Application;
+import ucar.nc2.NetcdfFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class IT_CDLConversion {
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(IT_CDLConversion.class);
 
-	public static void main(String[] args) {
-		LOG.debug("integration test running");
-
+	@Test
+	public void shouldConvertToReadableNetCDF() {
+		Application app = new Application();
+		try {
+			app.main(null);
+		} catch (Exception e) {
+			fail("Unexpected exception: " + e.getMessage());
+		}
+		String filename = "array_geo.nc";
+		NetcdfFile ncfile = null;
+		try {
+			assertNull(ncfile);
+			ncfile = NetcdfFile.open(filename);
+			// process( ncfile);
+		} catch (IOException ioe) {
+			fail("Unexpected exception: " + ioe.getMessage());
+			LOG.error("trying to open " + filename, ioe);
+		} finally {
+			if (null != ncfile)
+				try {
+					ncfile.close();
+					System.out.println(ncfile.toString());
+					LOG.debug(ncfile.toString());
+				} catch (IOException ioe) {
+					fail("Unexpected exception: " + ioe.getMessage());
+					LOG.error("trying to close " + filename, ioe);
+				}
+		}
 	}
-
 }
--- CDL_to_NetCDF_converter/src/main/java/uk/gov/meto/netcdf/cdl/Application.java
+++ CDL_to_NetCDF_converter/src/main/java/uk/gov/meto/netcdf/cdl/Application.java
@@ -1,11 +1,39 @@
 package uk.gov.meto.netcdf.cdl;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
 
-	public static void main(String[] args) {
-		System.out.println("Starting the main method");
-		
-		
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) throws Exception {
+		URL url = Application.class.getClass().getResource("/array_geo.cdl");
+		System.out.println("Starting the main method: " + url.getPath());
+		File cdlFile = new File(url.getPath());
+		System.out.println("exists: " + cdlFile.exists());
+		System.out.println(cdlFile.getCanonicalPath());
+		List<String> callParams = new ArrayList<String>(
+		        Arrays.asList("ncgen", "-o", "array_geo.nc", url.getPath()));
+		LOG.debug("Command is: {}", callParams);
+		Process process;
+		try {
+			System.out.println(new File(url.getPath()).exists());
+			ProcessBuilder processBuilder = new ProcessBuilder(callParams);
+			process = processBuilder.start();
+			int exitCode = process.waitFor();
+			System.out.println(new File("array_geo.nc").exists());
+		} catch (Exception e) {
+			LOG.error("process creation failed");
+			throw e;
+		}
 
 	}
 
