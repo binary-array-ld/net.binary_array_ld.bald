e6c869283b0de9d980dfd382a1ef6d8de1c48e85
Simon Oakes <simon.oakes@epimorphics.com>
2020-11-03 17:32:06 +0000

    Add alias support on CLI

--- binary-array-ld-cli/src/main/kotlin/net/bald/BinaryArrayConvertCli.kt
+++ binary-array-ld-cli/src/main/kotlin/net/bald/BinaryArrayConvertCli.kt
@@ -1,5 +1,8 @@
 package net.bald
 
+import net.bald.alias.AliasBinaryArray
+import net.bald.alias.AliasDefinition
+import net.bald.model.ModelAliasDefinition
 import net.bald.model.ModelBinaryArrayConverter
 import net.bald.netcdf.NetCdfBinaryArray
 import org.apache.commons.cli.DefaultParser
@@ -16,9 +19,10 @@ import kotlin.system.exitProcess
  */
 class BinaryArrayConvertCli {
     private val opts = Options().apply {
-        this.addOption("u", "uri", true, "The URI which identifies the dataset.")
-        this.addOption("c", "context", true, "Comma-delimited list of JSON-LD context files.")
-        this.addOption("h", "help", false, "Show help.")
+        addOption("u", "uri", true, "The URI which identifies the dataset.")
+        addOption("a", "alias", true, "RDF alias file.")
+        addOption("c", "context", true, "Comma-delimited list of JSON-LD context files.")
+        addOption("h", "help", false, "Show help.")
     }
 
     fun run(vararg args: String) {
@@ -36,8 +40,7 @@ class BinaryArrayConvertCli {
     }
 
     private fun doRun(opts: CommandLineOptions) {
-        val inputLoc = opts.inputLoc ?: throw IllegalArgumentException("First argument is required: NetCDF file to convert.")
-        val ba = NetCdfBinaryArray.create(inputLoc, opts.uri)
+        val ba = binaryArray(opts)
         val context = context(opts.contextLocs)
         val model = ba.use {
             ModelBinaryArrayConverter.convert(ba, context)
@@ -46,6 +49,23 @@ class BinaryArrayConvertCli {
         modelOutput(opts.outputLoc).use { output ->
             model.write(output, "ttl")
         }
+    }
+
+    private fun binaryArray(opts: CommandLineOptions): BinaryArray {
+        val inputLoc = opts.inputLoc ?: throw IllegalArgumentException("First argument is required: NetCDF file to convert.")
+        val ba = NetCdfBinaryArray.create(inputLoc, opts.uri)
+
+        val alias = opts.aliasLoc?.let(::alias)
+        return if (alias != null) {
+            AliasBinaryArray.create(ba, alias)
+        } else {
+            ba
+        }
+    }
+
+    private fun alias(aliasLoc: String): AliasDefinition {
+        val model = ModelFactory.createDefaultModel().read(aliasLoc)
+        return ModelAliasDefinition.create(model)
     }
 
     private fun context(contextLocs: List<String>): Model {
--- binary-array-ld-cli/src/test/kotlin/net/bald/BinaryArrayConvertCliTest.kt
+++ binary-array-ld-cli/src/test/kotlin/net/bald/BinaryArrayConvertCliTest.kt
@@ -1,15 +1,12 @@
 package net.bald
 
-import bald.jsonld.ContextReader
+import bald.jsonld.ResourceFileConverter
 import bald.model.ModelVerifier
 import bald.netcdf.CdlConverter.writeToNetCdf
 import net.bald.vocab.BALD
 import org.apache.jena.rdf.model.ModelFactory.createDefaultModel
 import org.apache.jena.rdf.model.ResourceFactory.*
-import org.apache.jena.vocabulary.DCTerms
-import org.apache.jena.vocabulary.RDF
-import org.apache.jena.vocabulary.SKOS
-import org.apache.jena.vocabulary.XSD
+import org.apache.jena.vocabulary.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.io.File
@@ -180,7 +177,10 @@ class BinaryArrayConvertCliTest {
     fun run_withExternalPrefixMapping_outputsPrefixMapping() {
         val inputFile = writeToNetCdf("/netcdf/prefix.cdl")
         val outputFile = createTempFile()
-        val contextFiles = listOf(ContextReader.toFile("/jsonld/context.json"), ContextReader.toFile("/jsonld/context2.json"))
+        val contextFiles = listOf(
+            ResourceFileConverter.toFile("/jsonld/context.json"),
+            ResourceFileConverter.toFile("/jsonld/context2.json")
+        )
 
         run(
             "--uri", "http://test.binary-array-ld.net/example",
@@ -212,11 +212,50 @@ class BinaryArrayConvertCliTest {
     fun run_withAttributes_outputsAttributes() {
         val inputFile = writeToNetCdf("/netcdf/attributes.cdl")
         val outputFile = createTempFile()
-        val contextFile = ContextReader.toFile("/jsonld/context.json")
+        val contextFile = ResourceFileConverter.toFile("/jsonld/context.json")
 
         run(
             "--uri", "http://test.binary-array-ld.net/example",
             "--context", contextFile.absolutePath,
+            inputFile.absolutePath,
+            outputFile.absolutePath
+        )
+
+        val model = createDefaultModel().read(outputFile.toURI().toString(), "ttl")
+        ModelVerifier(model).apply {
+            prefix("bald", BALD.prefix)
+            prefix("skos", SKOS.uri)
+            prefix("dct", DCTerms.NS)
+            resource("http://test.binary-array-ld.net/example/") {
+                statement(DCTerms.publisher, createResource("${BALD.prefix}Organisation"))
+                statement(createProperty("http://test.binary-array-ld.net/example//date"), createPlainLiteral("2020-10-29"))
+                statement(RDF.type, BALD.Container)
+                statement(SKOS.prefLabel, createPlainLiteral("Alias metadata example"))
+                statement(BALD.contains, model.createResource("http://test.binary-array-ld.net/example/var0")) {
+                    statement(RDF.type, BALD.Array)
+                    statement(RDF.type, BALD.Resource)
+                    statement(RDFS.label, createPlainLiteral("var-0"))
+                    statement(SKOS.prefLabel, createPlainLiteral("Variable 0"))
+                }
+                statement(BALD.contains, model.createResource("http://test.binary-array-ld.net/example/var1")) {
+                    statement(RDF.type, BALD.Resource)
+                }
+                statement(BALD.isPrefixedBy, createPlainLiteral("prefix_list"))
+            }
+        }
+    }
+
+    @Test
+    fun run_withAliases_outputsAliasedAttributes() {
+        val inputFile = writeToNetCdf("/netcdf/alias.cdl")
+        val outputFile = createTempFile()
+        val contextFile = ResourceFileConverter.toFile("/jsonld/context.json")
+        val aliasFile = ResourceFileConverter.toFile("/turtle/alias.ttl", "ttl")
+
+        run(
+            "--uri", "http://test.binary-array-ld.net/example",
+            "--context", contextFile.absolutePath,
+            "--alias", aliasFile.absolutePath,
             inputFile.absolutePath,
             outputFile.absolutePath
         )
