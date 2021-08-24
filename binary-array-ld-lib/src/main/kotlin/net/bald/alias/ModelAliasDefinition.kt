73fa5d6dfb1b943d10efb418cd7b79571e47f817
simonoakesepimorphics <37299842+simonoakesepimorphics@users.noreply.github.com>
2021-08-24 12:04:56 +0100

    Base vocab (#48)
    * Set theme jekyll-theme-slate
    
    * Create index.md
    
    Initialise GH pages
    
    * Add base vocab
    
    * Add base vocab to alias definition

--- binary-array-ld-lib/src/main/kotlin/net/bald/alias/ModelAliasDefinition.kt
+++ binary-array-ld-lib/src/main/kotlin/net/bald/alias/ModelAliasDefinition.kt
@@ -2,6 +2,7 @@ package net.bald.alias
 
 import net.bald.vocab.BALD
 import org.apache.jena.rdf.model.*
+import org.apache.jena.riot.RDFLanguages
 import org.apache.jena.vocabulary.DCTerms
 import org.apache.jena.vocabulary.OWL
 import org.apache.jena.vocabulary.RDF
@@ -76,7 +77,13 @@ class ModelAliasDefinition(
          */
         @JvmStatic
         fun create(model: Model): AliasDefinition {
-            return ModelAliasDefinition(model)
+            return ModelAliasDefinition(model.add(vocab))
+        }
+
+        private val vocab: Model by lazy {
+            this::class.java.getResourceAsStream("/bald/vocab/bald.ttl")?.use { input ->
+                ModelFactory.createDefaultModel().read(input, null, RDFLanguages.strLangTurtle)
+            } ?: throw IllegalStateException("Failed to load base vocabulary.")
         }
     }
 }
